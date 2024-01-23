1196a670b5e8fb9cee07f3e35d7188bc86de78ef
TiffanyNgoc <tiffanyngoc17@gmail.com>
2024-01-22 16:08:37 -0800

    Update main.cpp

--- lecture-materials/lecture_02/main.cpp
+++ lecture-materials/lecture_02/main.cpp
@@ -65,6 +65,18 @@ void drawScene(){
 
     glEnd();
 
+    glColor3f(1, 0, 1);
+
+     glBegin(GL_POLYGON);
+        glColor3f(1, 0, 0);
+        glVertex2f(0.5, 0.5);
+        glColor3f(0, 0, 1);
+        glVertex2f(0.5, 0.8);
+        glColor3f(1, 0, 1);
+        glVertex2f(0.1, 0.5);
+
+    glEnd();
+
     // We have been drawing to the back buffer, put it in the front
     glutSwapBuffers();
 }
