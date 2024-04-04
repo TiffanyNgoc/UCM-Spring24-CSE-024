a462d8e12cb4f6a902092b92f972bbbaf37a13f7
TiffanyNgoc <tiffanyngoc17@gmail.com>
2024-04-03 21:40:31 -0700

    Create Toolbar.h

---
+++ assignments/lab-6/exercise-1_1/Toolbar.h
@@ -0,0 +1,79 @@
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+#include "Rectangle.h"
+#include "Texture.h"
+#include "ColorPicker.h"
+
+enum Tool {PENCIL, ERASER};
+
+struct Toolbar {
+private:
+    Rectangle area;
+    Texture pencilButton;
+    Texture eraserButton;
+    Tool selectedTool;
+
+    RGBSlider redSlider;
+    RGBSlider greenSlider;
+    RGBSlider blueSlider;
+
+    void deselectAll(){
+        pencilButton.deselect();
+        eraserButton.deselect();
+    }
+
+public:
+    Toolbar(){
+        area = Rectangle(-1.0f, 1.0f, 0.2f, 2.0f, Color(0.8f, 0.8f, 0.8f));
+        pencilButton = Texture("assets/pencil.png", -1.0f, 1.0f, 0.2f, 0.2f);
+        eraserButton = Texture("assets/eraser.png", -1.0f, 0.8f, 0.2f, 0.2f);
+
+        selectPencil();
+
+        // Initialize RGB sliders
+        redSlider = RGBSlider(-0.9f, 0.5f, 1.6f, 0.05f, 1.0f);
+        greenSlider = RGBSlider(-0.9f, 0.3f, 1.6f, 0.05f, 1.0f);
+        blueSlider = RGBSlider(-0.9f, 0.1f, 1.6f, 0.05f, 1.0f);
+    }
+
+    Tool getSelectedTool(){
+        return selectedTool;
+    }
+
+    void selectPencil() {
+        deselectAll();
+        pencilButton.select();
+        selectedTool = PENCIL;
+    }
+
+    void selectEraser() {
+        deselectAll();
+        eraserButton.select();
+        selectedTool = ERASER;
+    }
+
+    void handleMouseClick(float x, float y){
+        if (pencilButton.contains(x, y)){
+            selectPencil();
+        } else if (eraserButton.contains(x, y)){
+            selectEraser();
+        }
+    }
+
+    void draw(){
+        area.draw();
+        pencilButton.draw();
+        eraserButton.draw();
+
+        redSlider.draw();
+        greenSlider.draw();
+        blueSlider.draw();
+    }
+
+    bool contains(float x, float y){
+        return area.contains(x, y);
+    }
+};
+
+#endif
