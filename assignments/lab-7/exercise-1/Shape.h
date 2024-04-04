cf605584635e28508814c4ec43327c5b32738e50
TiffanyNgoc <tiffanyngoc17@gmail.com>
2024-04-03 21:59:38 -0700

    Create Shape.h

---
+++ assignments/lab-7/exercise-1/Shape.h
@@ -0,0 +1,53 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include "Color.h"
+
+class Shape{
+protected:
+    float x;
+    float y;
+    bool selected;
+    Color color;
+
+public:
+    virtual void draw() = 0;
+    virtual bool contains(float, float) = 0;
+
+    virtual void setColor(const Color& newColor) {
+        color = newColor;
+    }
+
+    void select(){
+        selected = true;
+    }
+
+    void deselect(){
+        selected = false;
+    }
+
+    bool isSelected(){
+        return selected;
+    }
+
+    void setX(float x){
+        this->x = x;
+    }
+
+    void setY(float y){
+        this->y = y;
+    }
+
+    float getX(){
+        return x;
+    }
+
+    float getY(){
+        return y;
+    }
+
+    virtual ~Shape(){}
+
+};
+
+#endif
